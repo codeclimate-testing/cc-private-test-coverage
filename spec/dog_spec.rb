56a12e6bf85319b68bdb779c0aa7fabbf8a1a272
Noah Portes Chaikin <noah@porteschaikin.com>
2017-09-28 17:24:14 -0400

    airbnb unstable test coverage

--- spec/dog_spec.rb
+++ spec/dog_spec.rb
@@ -2,8 +2,8 @@ require "dog"
 
 RSpec.describe Dog do
   describe "#name" do
-    it "is super excited about its name" do
-      expect(Dog.new("Milo").name).to eq("Milo!!!")
+    it "is unexpected" do
+      expect(Dog.new("Milo").name).to eq("Cat")
     end
   end
 
