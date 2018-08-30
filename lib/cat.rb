ae15512e15b68bbccb00aadb87ce9b65a0f3b372
eutopian <eutopia@gmail.com>
2018-08-30 10:55:34 -0400

    diff

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,14 +3,6 @@ class Cat
     @name = name
   end
 
-  def untested
-    puts "no tests for this method yet"
-    puts "no tests for this method yet"
-    puts "no tests for this method yet"
-    puts "no tests for this method yet"
-    puts "no tests for this method yet"
-  end
-
   def untested_two
     puts "no tests for this method either"
     puts "no tests for this method either"
